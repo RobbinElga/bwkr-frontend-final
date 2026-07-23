24d96c194cc776333f47ca5136815b0d15b7dde2
Elga <elgafirmantara27@gmail.com>
2026-07-23 19:51:42 +0700

    fix eror

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,7 +2,6 @@ import { Header } from "@/components/layout/Header";
 import { Footer } from "@/components/layout/Footer";
 import { Fab } from "@/components/layout/Fab";
 import { BottomNav } from "@/components/layout/BottomNav";
-import { LandingSplash } from "@/components/ui/LandingSplash";
 import { getSiteSettings } from "@/services/public";
 
 export default async function PublicLayout({ children }: { children: React.ReactNode }) {
